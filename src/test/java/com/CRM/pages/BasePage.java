218d4a05a84cb7f5f46daac3dbee971e8ca691e5
aefpavlov <aefpavlov@gmail.com>
2024-03-19 07:28:13 -0700

    US1 Login

--- src/test/java/com/CRM/pages/BasePage.java
+++ src/test/java/com/CRM/pages/BasePage.java
@@ -1,17 +1,31 @@
 package com.CRM.pages;
 
 
+import com.CRM.utilities.ConfigurationReader;
 import com.CRM.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 
 public abstract class BasePage {
+
+    @FindBy(name = "USER_LOGIN")
+    public WebElement userNameInputBox;
+
+    @FindBy(name = "USER_PASSWORD")
+    public WebElement passwordInputBox;
+
+    @FindBy(className = "login-btn")
+    public WebElement logInBtn;
 
 
 
     public BasePage() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
+
+
 
 
 
--- src/test/java/com/CRM/pages/LoginPage.java
+++ src/test/java/com/CRM/pages/LoginPage.java
@@ -3,12 +3,32 @@ package com.CRM.pages;
 
 
 import com.CRM.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class LoginPage {
+public class LoginPage extends BasePage {
 
     public LoginPage(){
         PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+
+    @FindBy(className = "errortext")
+    public WebElement incorrectLoginMsg;
+
+    @FindBy(id = "USER_REMEMBER")
+    public WebElement rememberCheckBox;
+
+    @FindBy(className = "login-item-checkbox-label")
+    public WebElement rememberMeMessage;
+
+    public  void login( String userName, String password ){
+
+        this.userNameInputBox.clear();
+        this.userNameInputBox.sendKeys(userName);
+        this.passwordInputBox.sendKeys(password);
+        this.logInBtn.click();
     }
 
 
