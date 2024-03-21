a690e3884d1400a2565c26aed4c10d9deb6eaf1b
JOHANA403 <johaliza.23@gmail.com>
2024-03-21 10:42:16 -0700

    all tests pass

--- src/test/java/com/CRM/step_definitions/US11_AppreciationStepDefinitions.java
+++ src/test/java/com/CRM/step_definitions/US11_AppreciationStepDefinitions.java
@@ -3,6 +3,7 @@ package com.CRM.step_definitions;
 import com.CRM.pages.ActivityStreamPage;
 import com.CRM.pages.AppreciationPage;
 import com.CRM.pages.LoginPage;
+import com.CRM.utilities.BrowserUtils;
 import com.CRM.utilities.ConfigurationReader;
 import com.CRM.utilities.Driver;
 import io.cucumber.java.en.And;
@@ -15,30 +16,12 @@ import org.openqa.selenium.WebElement;
 
 public class US11_AppreciationStepDefinitions{
 
-    LoginPage loginPage = new LoginPage();
-
     ActivityStreamPage activityStreamPage = new ActivityStreamPage();
 
     AppreciationPage appreciationPage = new AppreciationPage();
 
 
-    @Given("user is on the login page of the application")
-    public void userIsOnTheLoginPageOfTheApplication() {
 
-        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
-    }
-
-    @When("user enters valid credentials")
-    public void userEntersValidCredentials() {
-
-        loginPage.login(ConfigurationReader.getProperty("hr_username"), ConfigurationReader.getProperty("hr_password"));
-    }
-    @Then("user should click Activity Stream")
-    public void user_should_click_activity_stream() {
-        activityStreamPage.activityStream.click();
-
-
-    }
     @When("user clicks on the more icon")
     public void userClicksOnTheMoreIcon() {
 
@@ -66,7 +49,7 @@ public class US11_AppreciationStepDefinitions{
     public void userTypesAnyAppreciationMessage() {
 
         Driver.getDriver().switchTo().frame(Driver.getDriver().findElement(By.xpath("//iframe[@class='bx-editor-iframe']")));
-        //activityStreamPage.messageBoxIframe.clear();
+        activityStreamPage.messageBoxIframe.clear();
         activityStreamPage.messageBoxIframe.sendKeys("Hi");
         Driver.getDriver().switchTo().defaultContent();
 
@@ -80,13 +63,16 @@ public class US11_AppreciationStepDefinitions{
 
     @Then("user reads {string}")
     public void userReads(String specifyPersonMsg) {
-        Assert.assertEquals("Please specify at least one person.", appreciationPage.specifyOnePersonMsg.getText());
+        BrowserUtils.sleep(3);
+        Assert.assertEquals("Please specify at least one person.", activityStreamPage.messageIsNotSpecifiedError.getText());
+
+
     }
 
     @Then("user sees the appreciation page with the mandatory field To: All employees by default")
     public void userSeesTheAppreciationPageWithTheMandatoryFieldToAllEmployeesByDefault() {
 
-        Assert.assertTrue(appreciationPage.allEmployeesCloseBtn.isDisplayed());
+        Assert.assertTrue(activityStreamPage.allEmployees.isDisplayed());
     }
     @Then("user clicks on the cancel button at any time")
     public void userClicksOnTheCancelButtonAtAnyTime() {
