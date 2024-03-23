9b953bfedb42b49def7ef73bbb9ac8cfd7614ed7
Leilacydeo <safavi_l@yahoo.com>
2024-03-23 13:55:11 -0700

    updated

--- src/test/java/com/CRM/pages/ActivityStreamPage.java
+++ src/test/java/com/CRM/pages/ActivityStreamPage.java
@@ -1,11 +1,13 @@
 package com.CRM.pages;
 
+import com.CRM.utilities.BrowserUtils;
 import com.CRM.utilities.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class ActivityStreamPage extends BasePage{
+public class ActivityStreamPage extends BasePage {
 
     public ActivityStreamPage() {
         PageFactory.initElements(Driver.getDriver(), this);
@@ -14,6 +16,7 @@ public class ActivityStreamPage extends BasePage{
 
     @FindBy(id = "user-name")
     public WebElement profilePage;// added by LG
+
     @FindBy(xpath = "//span[@id='feed-add-post-form-link-more']")
     public WebElement more;
 
@@ -22,7 +25,8 @@ public class ActivityStreamPage extends BasePage{
 
     @FindBy(xpath = "//span[@id='bx-b-uploadfile-blogPostForm']")
     public WebElement uploadFile;
-
+    @FindBy(xpath = "//img")
+    private WebElement picture;
     @FindBy(xpath = "//input[@name='bxu_files[]']")
     public WebElement uploadFileAndImages;
 
@@ -55,6 +59,7 @@ public class ActivityStreamPage extends BasePage{
     @FindBy(xpath = "//body[@contenteditable='true']")
     public WebElement messageBoxIframe;
 
+
     @FindBy(id = "blog-submit-button-cancel")
     public WebElement cancelButton;
 
@@ -79,8 +84,38 @@ public class ActivityStreamPage extends BasePage{
 
     @FindBy(xpath = "//*[contains(@id,'bxid')]")
     public WebElement uploadedFiles;
-    @FindBy(xpath="//td[@class='files-storage']")
+
+    @FindBy(xpath = "//tr[contains(@id,'disk-edit-attachn')]/td[1]/span/span[1]")
+    public WebElement insertedFile;
+    @FindBy(xpath = "//td[@class='files-storage']")
     public WebElement myDriveUploaded;
+    @FindBy(xpath = "//td[@class='files-name']")
+    public WebElement insertedFiles;
+    @FindBy(xpath = "//iframe[@class='bx-editor-iframe']")
+    public WebElement msgBoxFrame;
+    public void uploadFile(String fileName) {
+        String fileSeparator = System.getProperty("file.separator");
+        String path = System.getProperty("user.dir") + fileSeparator + "src/test/resources/files" + fileSeparator + fileName;
+         uploadFileAndImages.sendKeys(path);
+    }
+    public void switchToFrame(){
+        Driver.getDriver().switchTo().frame(Driver.getDriver().findElement(By.xpath("//iframe[@class='bx-editor-iframe']")));
+    }
+    public void switchToDefaultContent(){
+        Driver.getDriver().switchTo().defaultContent();
+    }
+
+    public String getPictureSrcFromMessage() {
+        switchToFrame();
+        BrowserUtils.waitFor(5);
+        String source=insertedFile.getAttribute("src");
+        switchToDefaultContent();
+        return source;
+    }
+    public String getMessageContent() {
+        switchToFrame();
+        String message=messageBox.getText();
+        switchToDefaultContent();
+        return message;
+    }
 }
-
-
